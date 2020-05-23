3074afa56dc55c145f9f937d7522f3253b799409
Николай Полтавский <unluck.bastard@gmail.com>
2020-05-23 14:52:04 +0300

    sup

--- TiCColorServer/menu.h
+++ TiCColorServer/menu.h
@@ -57,11 +57,11 @@ void help_menu_view() {
 	cout << "		m - man menu" << endl;
 	cout << "		? - help menu" << endl << endl;
 	cout << "	EXAMPLES: " << endl;
-	cout << "		dipscan.exe /s 0.0.0.0 8080" << endl;
-	cout << "		dipscan.exe /s 192.168.1.1 80" << endl << endl;
-	cout << "		dipscan.exe /f 111 111 111 or g300" << endl;
-	cout << "		dipscan.exe /h" << endl;
-	cout << "		dipscan.exe /?" << endl;
+	cout << "		TiKKColorServer.exe /s 0.0.0.0 8080" << endl;
+	cout << "		TiKKColorServer.exe /s 192.168.1.1 80" << endl << endl;
+	cout << "		TiKKColorServer.exe /f 111 111 111 or g300" << endl;
+	cout << "		TiKKColorServer.exe /h" << endl;
+	cout << "		TiKKColorServer.exe /?" << endl;
 }
 
 void hidecursor() {
